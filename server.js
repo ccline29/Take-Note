41d28a991ffad73916e865f5f2ffd4798d4fe7f7
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-02-20 19:52:49 -0600

    modify package

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoutes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// middlewares
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -15,5 +15,5 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-// spin up server
+// default route
 app.listen(PORT, () => console.log(`Your app is running at http://localhost:${PORT}`))
